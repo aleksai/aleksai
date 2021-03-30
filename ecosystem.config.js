c34b4605816eb8b4754e1e244f45a9f0572c3709
Alexei Pugachev <lenyapugachev@me.com>
2021-03-30 14:55:18 +0700

    Separate prod app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,19 @@ module.exports = {
       env: {
         NODE_ENV: 'dev',
       },
-      env_production: {
-        NODE_ENV: 'production',
-      },
       env_staging: {
         NODE_ENV: 'staging',
+      },
+    },
+    {
+      name: 'ALEKSAI-PROD',
+      script: 'dist/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
       },
     },
   ],
