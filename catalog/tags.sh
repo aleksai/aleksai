e8d1c0e7c27a4f48838ef9483ed4ee93ae4aa712
Alek Sai <lenyapugachev@me.com>
2023-11-28 20:57:40 +0300

    Refactor 2023 III + update catalog

--- catalog/tags.sh
+++ catalog/tags.sh
@@ -2,6 +2,10 @@
 
 for dir in */; do
     cd "$dir" || exit
-    tag -l . | awk '{$1=""; sub(/^[[:space:]]*/, ""); sub(/[[:space:]]*$/, ""); sub(/\.$/, ""); print $0}' > tags.txt
+    for subdir in */; do
+        cd "$subdir" || exit
+        tag -l . | awk '{$1=""; sub(/^[[:space:]]*/, ""); sub(/[[:space:]]*$/, ""); sub(/\.$/, ""); print $0}' > tags.txt
+        cd ..
+    done
     cd ..
 done
